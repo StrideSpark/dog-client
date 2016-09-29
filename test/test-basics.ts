815fa4af93054d130053ab3513f53cb44850790c
Megan Schoendorf <megan.schoendorf@gmail.com>
2016-09-29 14:04:43 -0700

    switch to env test

--- test/test-basics.ts
+++ test/test-basics.ts
@@ -5,6 +5,8 @@
 import DogClient from '../index';
 import {Response} from "../index";
 import {assert} from 'chai';
+import {ENV_TEST} from "@stridespark/model-core";
+
 
 let AWS = require('aws-sdk');
 AWS.config.region = 'us-west-2';
@@ -12,7 +14,7 @@ AWS.config.region = 'us-west-2';
 describe("basic test", function() {
     it("mocks correctly", async function() {
         let client = new DogClient();
-        assert.equal(Response.MOCKED, await client.initDogAPI("test", ["tag:1"], "test.prefix", "testhost", true));
+        assert.equal(Response.MOCKED, await client.initDogAPI(ENV_TEST, ["tag:1"], "test.prefix", "testhost", true));
         assert.equal(Response.MOCKED, await client.sendCountOne("fake.metric"));
         assert.equal(Response.MOCKED, await client.sendCount("fake.metric", 2));
         assert.equal(Response.MOCKED, await client.sendCountWithTags("fake.metric", 5, ["tag:2"]));
@@ -21,7 +23,7 @@ describe("basic test", function() {
     it("real", async function() {
         this.timeout(10000);
         let client = new DogClient();
-        assert.equal(Response.OK, await client.initDogAPI("development", ["tag:1"], "development.prefix", "testhost", false));
+        assert.equal(Response.OK, await client.initDogAPI(ENV_TEST, ["tag:1"], "development.prefix", "testhost", false));
         assert.equal(Response.OK, await client.sendCountOne("fake.metric"));
         assert.equal(Response.OK, await client.sendCount("fake.metric", 2));
         assert.equal(Response.OK, await client.sendCountWithTags("fake.metric", 5, ["tag:2"]));
