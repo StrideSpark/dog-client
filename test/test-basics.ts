2df169c31698eb5ce66e20c2288a69239eec1f3f
Megan <megan@stridespark.com>
2017-01-25 12:17:55 -0800

    Add mock data (#1)
    * added mock data to datadog client
    
    * 2.2.0

--- test/test-basics.ts
+++ test/test-basics.ts
@@ -12,11 +12,32 @@ AWS.config.region = 'us-west-2';
 describe("basic test", function () {
     it("mocks correctly", async function () {
         let client = new DogClient();
-        assert.equal(Response.MOCKED, await client.initDogAPI('test', ["tag:1"], "test.prefix", "testhost", true));
-        assert.equal(Response.MOCKED, await client.sendCountOne("fake.metric"));
-        assert.equal(Response.MOCKED, await client.sendCount("fake.metric", 2));
-        assert.equal(Response.MOCKED, await client.sendCountWithTags("fake.metric", 5, ["tag:2"]));
-        assert.equal(Response.MOCKED, await client.sendGauge("fake.gauge", 5));
+        assert.equal(await client.initDogAPI('test', ["tag:1"], "test.prefix", "testhost", true), Response.MOCKED, '1');
+
+        //send a metric, make sure it is mocked and the mock data has the right stuff
+        assert.equal(await client.sendCountOne("fake.metric"), Response.MOCKED, '2');
+        assert.equal(client.getMetric('fake.metric', 'tag:1'), 1, '3');
+        assert.equal(client.getMetric('fake.metric', 'env:test'), 1, '4');
+
+        //and repeat...        
+        assert.equal(await client.sendCount("fake.metric", 2), Response.MOCKED);
+        assert.equal(client.getMetric('fake.metric', 'tag:1'), 3);
+        assert.equal(client.getMetric('fake.metric', 'env:test'), 3);
+
+        //and repeat...        
+        assert.equal(await client.sendCountWithTags("fake.metric", 5, ["tag:2"]), Response.MOCKED);
+        assert.equal(client.getMetric('fake.metric', 'tag:1'), 8);
+        assert.equal(client.getMetric('fake.metric', 'env:test'), 8);
+        assert.equal(client.getMetric('fake.metric', 'tag:2'), 5);
+
+        //and do a gauge        
+        assert.equal(await client.sendGauge("fake.gauge", 5), Response.MOCKED);
+        assert.equal(client.getMetric('fake.gauge', 'tag:1'), 5);
+        assert.equal(client.getMetric('fake.gauge', 'env:test'), 5);
+
+        //and clear:
+        client.clearMockData();
+        assert.equal(client.getMetric('fake.gauge', 'tag:1'), 0);
     });
 
     it("real", async function () {
