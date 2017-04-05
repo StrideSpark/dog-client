20305ff82eb0c7dc3323bc36643cb1faaec22e22
Scott Bessler <scott@stridespark.com>
2017-04-05 14:36:37 -0700

    use dogstatsd (#2)
    * use dogstatsd
    
    * 3.0.0
    
    * pick nits
    
    * remove unused deps

--- index.ts
+++ index.ts
@@ -1,9 +1,41 @@
-/**
- * Created by meganschoendorf on 6/3/16.
- */
-
 import { fetchCred } from 'credstash-promise';
-let dogapi = require('dogapi');
+const HotShots = require('hot-shots');
+
+type statsdFunction = (
+    /**
+     * name: Stat name (required)
+     */
+    name: string,
+
+    /**
+     * value: Stat value required except in increment/decrement where it defaults to 1/-1 respectively
+     */
+    value: number,
+
+    /**
+     * sampleRate: Sends only a sample of data to StatsD default: 1
+     */
+    sampleRate?: number,
+
+    /**
+     * tags: The Array of tags to add to metrics default: []
+     */
+    tags?: string[],
+
+    /**
+     * callback: The callback to execute once the metric has been sent or buffered
+     */
+    cb?: (err: any, bytes: number) => void
+) => void;
+
+declare interface StatsD {
+    increment: statsdFunction
+    decrement: statsdFunction
+    gauge: statsdFunction
+    histogram: statsdFunction
+    close: (cb?: (err: any) => void) => void
+}
+
 
 export enum Response {
     OK,
@@ -15,60 +47,32 @@ export interface MockData {
     [index: string]: { [tag: string]: number }
 }
 
+const DEFAULT_SAMPLE_RATE = 1;
 export default class DogClient {
     private tags: Array<string> = [];
-    private prefix: string;
     private host: string;
-    private standardOptions: { [index: string]: any };
-    private standardGaugeOptions: { [index: string]: any };
     private mock: boolean = false;
     private mockData: MockData = {}
+    private _client: StatsD;
 
-    async initDogAPI(env: string, tags: Array<string>, prefix: string, host: string, mock: boolean): Promise<Response> {
-        this.prefix = prefix;
+    initDogAPI(env: string, tags: Array<string>, prefix: string, host: string, mock: boolean) {
+        const statsdHost = process.env.KUBERNETES_SERVICE_HOST != undefined ? 'datadog-statsd.default' : 'localhost';
+        this.tags = tags;
+        this._client = new HotShots({
+            host: statsdHost,
+            prefix: prefix.slice(-1) === '.' ? prefix : prefix + '.',
+            // not using globaltags because of mock data:
+            // globalTags: tags.concat('env:' + env),
+        }) as StatsD;
         this.host = host;
-
-        tags.forEach(a => this.tags.push(a));
-        if (this.tags.indexOf('env:' + env) < 0) {
-            this.tags.push('env:' + env);
-        }
-
-        this.standardOptions = {
-            host: this.host,
-            tags: this.tags,
-            type: "count"
-        };
-        this.standardGaugeOptions = Object.assign({}, this.standardOptions, { type: 'gauge' });
-
+        this.tags = tags.concat('env:' + env)
         //mock means that nothing gets fired
         if (mock) {
             this.mock = true;
             return Response.MOCKED;
         }
 
-        let keys: Array<string> = await Promise.all([
-            fetchCred(env + ".datadog.appkey"),
-            fetchCred(env + ".datadog.apikey")
-        ]);
-
-        let app_key: string = keys[0];
-        let api_key: string = keys[1];
-
-        if (app_key && api_key) {
-            dogapi.initialize({
-                api_key: api_key,
-                app_key: app_key
-            });
-            return Response.OK;
-
-        } else {
-            console.error("Could not load data dog creds from credstash");
-            return Response.ERROR;
-        }
-    }
-
-    addPrefix(prefix: string) {
-        this.prefix += '.' + prefix;
+        return Response.OK;
     }
 
     addTags(tags: string[]) {
@@ -77,69 +81,84 @@ export default class DogClient {
         })
     }
 
-    sendCountOne(metric: string): Promise<Response> {
+    sendCountOne(metric: string) {
         if (this.mock) {
             this._addToMockData(metric, 1, []);
-            return Promise.resolve(Response.MOCKED);
+            return;
         }
-        return this._send(metric, 1, this.standardOptions);
+        this._client.increment(metric, 1, DEFAULT_SAMPLE_RATE);
     }
 
-    sendCountOneWithTags(metric: string, tags: Array<string>): Promise<Response> {
+    sendCountOneWithTags(metric: string, tags: Array<string>) {
         if (this.mock) {
             this._addToMockData(metric, 1, tags);
-            return Promise.resolve(Response.MOCKED);
+            return;
         }
-        return this._send(metric, 1, { host: this.host, tags: this.tags.concat(tags), type: "count" });
+
+        this._client.increment(metric, 1, DEFAULT_SAMPLE_RATE, tags.concat(this.tags));
     }
 
-    sendCount(metric: string, count: number): Promise<Response> {
+    /**
+     * this is specifically for recording failures while exiting
+     * @param metric
+     * @param tags
+     */
+    async sendCountOneAndClose(metric: string, tags: Array<string> = []) {
+        if (this.mock) {
+            this._addToMockData(metric, 1, tags);
+            return 0;
+        }
+
+        return new Promise<number>(
+            (resolve, reject) => {
+                this._client.increment(metric, 1, DEFAULT_SAMPLE_RATE, tags, (err, bytes) => {
+                    if (err) reject(err);
+                    this._client.close(err => {
+                        if (err) { console.error(err, 'failed to close statsd') }
+                        resolve(bytes);
+                    })
+                });
+            });
+    }
+
+    sendCount(metric: string, count: number) {
         if (this.mock) {
             this._addToMockData(metric, count, []);
-            return Promise.resolve(Response.MOCKED);
+            return;
         }
-        return this._send(metric, count, this.standardOptions);
+        this._client.increment(metric, count);
     }
 
-    sendCountWithTags(metric: string, count: number, tags: Array<string>): Promise<Response> {
+    sendCountWithTags(metric: string, count: number, tags: Array<string>) {
         if (this.mock) {
             this._addToMockData(metric, count, tags);
-            return Promise.resolve(Response.MOCKED);
+            return;
         }
-        return this._send(metric, count, { host: this.host, tags: this.tags.concat(tags), type: "count" });
+        this._client.increment(metric, count, DEFAULT_SAMPLE_RATE, tags.concat(this.tags));
     }
 
-    sendGauge(metric: string, value: number): Promise<Response> {
+    sendGauge(metric: string, value: number) {
         if (this.mock) {
             this._addToMockData(metric, value, []);
-            return Promise.resolve(Response.MOCKED);
+            return;
         }
-        return this._send(metric, value, this.standardGaugeOptions);
+        this._client.gauge(metric, value);
     }
 
-    sendGaugeWithTags(metric: string, value: number, tags: Array<string>): Promise<Response> {
+    sendGaugeWithTags(metric: string, value: number, tags: Array<string>) {
         if (this.mock) {
             this._addToMockData(metric, value, tags);
-            return Promise.resolve(Response.MOCKED);
+            return;
         }
-        return this._send(metric, value, { host: this.host, tags: this.tags.concat(tags), type: "gauge" });
+        this._client.gauge(metric, value, DEFAULT_SAMPLE_RATE, tags.concat(this.tags));
     }
 
-    private _send(metric: string, count: number, options: any): Promise<Response> {
-        return new Promise<Response>(
-            (resolve, reject) =>
-                dogapi.metric.send(this._getFullMetric(metric), count, options, (err: Error, resp: any) => {
-                    if (err) {
-                        console.error(err);
-                        resolve(Response.ERROR);
-                    }
-                    else resolve(Response.OK);
-                })
-        );
-    }
-
-    private _getFullMetric(metric: string): string {
-        return this.prefix + "." + metric;
+    histogram(metric: string, value: number, tags: Array<string>) {
+        if (this.mock) {
+            this._addToMockData(metric, value, tags);
+            return;
+        }
+        this._client.histogram(metric, value, DEFAULT_SAMPLE_RATE, tags.concat(this.tags));
     }
 
     private _addToMockData(metric: string, count: number, tags: Array<string>) {
--- test/test-basics.ts
+++ test/test-basics.ts
@@ -10,28 +10,28 @@ let AWS = require('aws-sdk');
 AWS.config.region = 'us-west-2';
 
 describe("basic test", function () {
-    it("mocks correctly", async function () {
+    it("mocks correctly", function () {
         let client = new DogClient();
-        assert.equal(await client.initDogAPI('test', ["tag:1"], "test.prefix", "testhost", true), Response.MOCKED, '1');
+        client.initDogAPI('test', ["tag:1"], "test.prefix", "testhost", true);
 
         //send a metric, make sure it is mocked and the mock data has the right stuff
-        assert.equal(await client.sendCountOne("fake.metric"), Response.MOCKED, '2');
+        client.sendCountOne("fake.metric"), Response.MOCKED;
         assert.equal(client.getMetric('fake.metric', 'tag:1'), 1, '3');
         assert.equal(client.getMetric('fake.metric', 'env:test'), 1, '4');
 
-        //and repeat...        
-        assert.equal(await client.sendCount("fake.metric", 2), Response.MOCKED);
+        //and repeat...
+        client.sendCount("fake.metric", 2);
         assert.equal(client.getMetric('fake.metric', 'tag:1'), 3);
         assert.equal(client.getMetric('fake.metric', 'env:test'), 3);
 
-        //and repeat...        
-        assert.equal(await client.sendCountWithTags("fake.metric", 5, ["tag:2"]), Response.MOCKED);
+        //and repeat...
+        client.sendCountWithTags("fake.metric", 5, ["tag:2"]);
         assert.equal(client.getMetric('fake.metric', 'tag:1'), 8);
         assert.equal(client.getMetric('fake.metric', 'env:test'), 8);
         assert.equal(client.getMetric('fake.metric', 'tag:2'), 5);
 
-        //and do a gauge        
-        assert.equal(await client.sendGauge("fake.gauge", 5), Response.MOCKED);
+        //and do a gauge
+        client.sendGauge("fake.gauge", 5);
         assert.equal(client.getMetric('fake.gauge', 'tag:1'), 5);
         assert.equal(client.getMetric('fake.gauge', 'env:test'), 5);
 
@@ -43,10 +43,12 @@ describe("basic test", function () {
     it("real", async function () {
         this.timeout(10000);
         let client = new DogClient();
-        assert.equal(Response.OK, await client.initDogAPI('test', ["tag:1"], "development.prefix", "testhost", false));
-        assert.equal(Response.OK, await client.sendCountOne("fake.metric"));
-        assert.equal(Response.OK, await client.sendCount("fake.metric", 2));
-        assert.equal(Response.OK, await client.sendCountWithTags("fake.metric", 5, ["tag:2"]));
-        assert.equal(Response.OK, await client.sendGauge("fake.gauge", 5));
+        client.initDogAPI('test', ["tag:1"], "development.prefix", "testhost", false);
+        client.sendCountOne("fake.metric");
+        client.sendCount("fake.metric", 2);
+        client.sendCountWithTags("fake.metric", 5, ["tag:2"]);
+        client.sendGauge("fake.gauge", 5);
+
+        await client.sendCountOneAndClose('error')
     });
 });
